99c5b5170e727f9d0e55425c412ff44d3b163f25
Kevin <mr.kev@me.com>
2014-11-11 21:33:57 -0500

    Oh yeah, and add geographical coordinates

--- example.js
+++ example.js
@@ -3,7 +3,9 @@
 var iroh = require('./index');
 
 iroh.getJSON('north_star').then(function (data) {
-  console.dir (JSON.stringify(data.events))
+  // console.dir (JSON.stringify(data.events))
+  console.dir (data)
+
   // data.events.forEach(function (evt) {
   //   console.log(evt)
   // });
